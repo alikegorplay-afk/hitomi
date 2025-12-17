4537176096fc7436fbec40bf3bdeef7a661c79c0
GameHipe <alikegorplay@gmail.com>
2025-12-17 22:50:08 +0900

    logger-update

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ import aiohttp
 from loguru import logger
 from src.bot import start_bot
 
+logger.remove()
+logger.add(
+    "logs.log",
+    format = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    level="INFO",
+    rotation="10 MB",
+)
+
 async def main():
     async with aiohttp.ClientSession() as session:
         await start_bot(session)
-        
         
 if __name__ == '__main__':
     try:
